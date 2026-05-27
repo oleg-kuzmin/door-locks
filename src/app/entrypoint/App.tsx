5f5a0318ff2ca25c15bd2cef80e10c960cee3f19
Oleg Kuzmin <info@oleg-kuzmin.ru>
2026-05-27 19:40:25 +0300

    init: add fonts

--- src/app/entrypoint/App.tsx
+++ src/app/entrypoint/App.tsx
@@ -1,10 +1,5 @@
-import { Test } from '@/shared/ui/Test';
 import styles from './App.module.scss';
 
 export function App() {
-  return (
-    <div className={styles.app}>
-      <Test />
-    </div>
-  );
+  return <div className={styles.app}>Door locks</div>;
 }
