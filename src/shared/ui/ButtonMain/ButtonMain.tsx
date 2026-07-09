c252e8276e022e37fdc25fa138e10f098a25e5c5
Oleg Kuzmin <info@oleg-kuzmin.ru>
2026-07-09 13:02:16 +0300

    refactor: add Link variation to shared/ButtonMain

--- src/shared/ui/ButtonMain/ButtonMain.tsx
+++ src/shared/ui/ButtonMain/ButtonMain.tsx
@@ -1,13 +1,23 @@
+import { Link } from 'react-router';
 import cn from 'classnames';
 import styles from './ButtonMain.module.scss';
 
 interface ButtonMainProps {
   children: string;
-  onClick: VoidFunction;
+  onClick?: VoidFunction;
+  to?: string;
   className?: string;
 }
 
-export function ButtonMain({ children, onClick, className }: Readonly<ButtonMainProps>) {
+export function ButtonMain({ children, to, onClick, className }: Readonly<ButtonMainProps>) {
+  if (to) {
+    return (
+      <Link className={cn(styles.buttonMain, className)} to={to}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
     <button className={cn(styles.buttonMain, className)} onClick={onClick}>
       {children}
