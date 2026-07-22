4981beb31391354b5b859ad09ad1c9748c21dc86
Oleg Kuzmin <info@oleg-kuzmin.ru>
2026-07-22 13:36:26 +0300

    feat: add CatalogImage to header/CatalogNav

--- src/widgets/header/ui/CatalogNav/CatalogNav.tsx
+++ src/widgets/header/ui/CatalogNav/CatalogNav.tsx
@@ -5,14 +5,14 @@ import { ButtonMain } from '@/shared/ui';
 import styles from './CatalogNav.module.scss';
 
 const catalogList = [
-  { name: 'Накладные электронные замки', to: '/' },
-  { name: 'Врезные электронные замки', to: '/' },
-  { name: 'Замки для квартиры', to: '/' },
-  { name: 'Замки для дома', to: '/' },
-  { name: 'Замки для отелей', to: '/' },
-  { name: 'Замки для офиса', to: '/' },
-  { name: 'Замки для шкафчиков', to: '/' },
-  { name: 'Замки для раздевалок', to: '/' },
+  { name: 'Накладные электронные замки', to: '/', image: '/categories/surface-electronic.png' },
+  { name: 'Врезные электронные замки', to: '/', image: '/categories/mortise-electronic.png' },
+  { name: 'Замки для квартиры', to: '/', image: '/categories/apartment.png' },
+  { name: 'Замки для дома', to: '/', image: '/categories/home.png' },
+  { name: 'Замки для отелей', to: '/', image: '/categories/hotel.png' },
+  { name: 'Замки для офиса', to: '/', image: '/categories/office.png' },
+  { name: 'Замки для шкафчиков', to: '/', image: '/categories/cabinet.png' },
+  { name: 'Замки для раздевалок', to: '/', image: '/categories/changing-rooms.png' },
 ];
 
 interface CatalogNavProps {
@@ -20,7 +20,11 @@ interface CatalogNavProps {
 }
 
 export function CatalogNav({ className }: Readonly<CatalogNavProps>) {
-  const [activeName, setActiveName] = useState(catalogList.at(0)?.name);
+  const [activeName, setActiveName] = useState(catalogList.at(0)?.name || null);
+  const imageSrc = catalogList.find(catalog => catalog.name === activeName)?.image || null;
+
+  console.log(activeName);
+  console.log(imageSrc);
 
   function handleActiveElement(name: string) {
     setActiveName(name);
@@ -38,14 +42,23 @@ export function CatalogNav({ className }: Readonly<CatalogNavProps>) {
             onActive={handleActiveElement}
           />
         ))}
+        <li>
+          <ButtonMain
+            className={styles.catalogNav__buttonMain}
+            to="/"
+            onMouseEnter={() => setActiveName(null)}
+            onFocus={() => setActiveName(null)}>
+            Смотреть все
+          </ButtonMain>
+        </li>
       </ul>
-      <ButtonMain
-        className={styles.catalogNav__buttonMain}
-        to="/"
-        onMouseEnter={() => setActiveName('')}
-        onFocus={() => setActiveName('')}>
-        Смотреть все
-      </ButtonMain>
+      <CatalogImageContainer>
+        <CatalogImage
+          key={activeName}
+          src={imageSrc ?? '/categories/default.png'}
+          alt={activeName ?? 'смотреть все'}
+        />
+      </CatalogImageContainer>
     </nav>
   );
 }
@@ -60,12 +73,12 @@ interface CatalogElementProps {
 
 function CatalogElement({ name, to, isActive, onActive }: Readonly<CatalogElementProps>) {
   return (
-    <li
-      className={styles.catalogElement}
-      key={name}
-      onMouseEnter={() => onActive(name)}
-      onFocus={() => onActive('')}>
-      <Link className={styles.catalogElement__link} to={to}>
+    <li className={styles.catalogElement} key={name}>
+      <Link
+        className={styles.catalogElement__link}
+        to={to}
+        onMouseEnter={() => onActive(name)}
+        onFocus={() => onActive(name)}>
         {name}
       </Link>
       <div
@@ -74,4 +87,23 @@ function CatalogElement({ name, to, isActive, onActive }: Readonly<CatalogElemen
         })}></div>
     </li>
   );
+}
+
+// CatalogImageContainer
+interface CatalogImageContainerProps {
+  children: React.ReactNode;
+}
+
+function CatalogImageContainer({ children }: Readonly<CatalogImageContainerProps>) {
+  return <div className={styles.catalogImageContainer}>{children}</div>;
+}
+
+// CatalogImage
+interface CatalogImageProps {
+  src: string;
+  alt: string;
+}
+
+function CatalogImage({ src, alt }: Readonly<CatalogImageProps>) {
+  return <img className={styles.catalogImage} src={src} alt={alt} />;
 }
