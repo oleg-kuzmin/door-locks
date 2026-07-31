dbf2ef899179b8622ac3d5b9a676e42be4bc9405
Oleg Kuzmin <info@oleg-kuzmin.ru>
2026-07-31 12:56:19 +0300

    feat: add Pagination to Hero

--- src/pages/home/ui/Hero/Hero.tsx
+++ src/pages/home/ui/Hero/Hero.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames';
-import { HeroCard } from '@/entities/hero-card';
+// import { HeroCard } from '@/entities/hero-card';
 import { Content } from '@/shared/ui';
 import styles from './Hero.module.scss';
 
@@ -11,8 +11,53 @@ export function Hero({ className }: Readonly<HeroProps>) {
   return (
     <section className={cn(styles.hero, className)}>
       <Content className={styles.hero__content}>
-        <HeroCard />
+        {/* <HeroCard /> */}
+        <Pagination />
       </Content>
     </section>
   );
+}
+
+function Pagination() {
+  return (
+    <div className={styles.pagination}>
+      <ButtonSlide type="prev" />
+      <div className={styles.pagination__bullets}>
+        <PaginationBullet />
+        <PaginationBullet isActive />
+        <PaginationBullet />
+      </div>
+      <ButtonSlide type="next" />
+    </div>
+  );
+}
+
+interface PaginationBulletProps {
+  isActive?: boolean;
+}
+
+function PaginationBullet({ isActive }: Readonly<PaginationBulletProps>) {
+  return (
+    <div
+      className={cn(styles.paginationBullet, { [styles.paginationBullet_active]: isActive })}></div>
+  );
+}
+
+interface ButtonSlideProps {
+  type: 'prev' | 'next';
+}
+
+function ButtonSlide({ type }: Readonly<ButtonSlideProps>) {
+  let className = styles.buttonSlide;
+
+  switch (type) {
+    case 'prev':
+      className = cn(className, styles.buttonSlide_prev);
+      break;
+    case 'next':
+      className = cn(className, styles.buttonSlide_next);
+      break;
+  }
+
+  return <button className={className}></button>;
 }
