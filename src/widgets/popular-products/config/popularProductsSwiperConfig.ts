d0e237b81240e661b8bb95a2664607997f2480c3
Oleg Kuzmin <info@oleg-kuzmin.ru>
2026-09-12 12:04:02 +0300

    styles: change config PopularProducts

--- src/widgets/popular-products/config/popularProductsSwiperConfig.ts
+++ src/widgets/popular-products/config/popularProductsSwiperConfig.ts
@@ -6,26 +6,23 @@ export const popularProductsSwiperConfig: SwiperProps = {
   speed: 1000,
   watchSlidesProgress: true,
   breakpointsBase: 'container',
+  centeredSlides: false,
   breakpoints: {
     0: {
       slidesPerGroup: 1,
       allowTouchMove: true,
-      centeredSlides: false,
     },
     636: {
       slidesPerGroup: 2,
       allowTouchMove: true,
-      centeredSlides: false,
     },
     954: {
       slidesPerGroup: 3,
       allowTouchMove: true,
-      centeredSlides: false,
     },
     1272: {
       slidesPerGroup: 4,
       allowTouchMove: false,
-      centeredSlides: false,
     },
   },
 };
